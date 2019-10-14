dab9fe48f950c82dc42287e916ae8a8b7248075a
Hanif Ali <alihanif016@gmail.com>
2019-10-14 21:41:49 +0500

    Designed Join Page Moved some stylings from login.css to the global style.css Added join.css for specific stylings

--- project2/application.py
+++ project2/application.py
@@ -5,13 +5,24 @@ app = Flask(__name__)
 
 socket = SocketIO(app)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route("/join", methods=["POST"])
+@app.route("/join", methods=["POST", "GET"])
 def join():
     return render_template("join.html")
 
